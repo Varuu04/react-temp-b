dc440fe5bc430eaacba88115008840b2a17e151c
Varuu04 <vardhanahemsham22@gmail.com>
2025-04-02 15:26:02 +0530

    updated

---
+++ src/components/App16.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useRef } from "react";
+export default function App1() {
+    let numRef=useRef();
+    const handleSubmit=()=>{
+        let num=numRef.current.value
+        if(num>18){
+            alert("Welcome")
+        }else{
+            alert("Not allowed")
+        }
+    }
+  return (
+    <div>
+      <h1>This is App16</h1>
+      <p><input type="number" ref={numRef}></input></p>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
